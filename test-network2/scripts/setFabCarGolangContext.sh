3c21387538af1d540863384accad803c98d9f356
SathvikaP <sathvikapallapothula@gmail.com>
2024-10-07 12:42:26 +0530

    added fabcar

--- test-network2/scripts/setFabCarGolangContext.sh
+++ test-network2/scripts/setFabCarGolangContext.sh
@@ -1,9 +1,9 @@
 export CC_RUNTIME_LANGUAGE=golang
-export CC_SRC_PATH="../chaincode/fabcar/go/"
+export CC_SRC_PATH="../asset-transfer-basic/chaincode-go"
 export VERSION=1
 
 echo Vendoring Go dependencies ...
-pushd ../chaincode/fabcar/go
+pushd ../asset-transfer-basic/chaincode-go"
 export GO111MODULE=on go mod vendor
 popd
 echo Finished vendoring Go dependencies
