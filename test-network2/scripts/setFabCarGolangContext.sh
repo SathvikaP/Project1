dce8ee2767009793e48a2745899282e7f724b7af
SathvikaP <110251157+SathvikaP@users.noreply.github.com>
2024-10-07 13:00:46 +0530

    Update setFabCarGolangContext.sh

--- test-network2/scripts/setFabCarGolangContext.sh
+++ test-network2/scripts/setFabCarGolangContext.sh
@@ -3,7 +3,7 @@ export CC_SRC_PATH="../asset-transfer-basic/chaincode-go"
 export VERSION=1
 
 echo Vendoring Go dependencies ...
-pushd ../asset-transfer-basic/chaincode-go"
+pushd ../asset-transfer-basic/chaincode-go
 export GO111MODULE=on go mod vendor
 popd
 echo Finished vendoring Go dependencies
